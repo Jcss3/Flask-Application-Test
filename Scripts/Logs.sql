d0507cea8f4a1b9ab6261774a97911e00abb58e1
Jcss3 <39499186+Jcss3@users.noreply.github.com>
2019-12-29 18:50:48 -0300

    update log.sql

--- Scripts/Logs.sql
+++ Scripts/Logs.sql
@@ -5,7 +5,7 @@ log_senha varchar(200) not null,
 log_email varchar(200) not null,
 log_hora_Registro timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-# Triggers para qualquer mudança na Tabela de informações
+# Triggers para qualquer mudança na Tabela de usuario
 create trigger tr_log_Insercao after insert on Usuario for each row
 insert into Registros (log_acao,log_login,log_senha,log_email) values ('Criação de Usuário',New.login,New.senha,New.email);
 
